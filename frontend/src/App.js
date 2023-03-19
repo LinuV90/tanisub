22f4b84b8c07d254596ba77899bf43503e7db488
LinuV90 <e.elisidik@gmail.com>
2023-03-19 11:20:54 +0700

    video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">tanisub</a>
+      </header>
+      <main>daftar produk</main>
+    </div>
+  );
+}
+
+export default App;
